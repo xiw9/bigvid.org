e1003312b6b48a219b2962cfbb21f12ed93ecd70
Xi Wang <wandsea@gmail.com>
2014-12-14 22:46:03 +0800

    dataset

--- dataset.php
+++ dataset.php
@@ -1,13 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php include 'header.php'; print_header('Dataset - BigVid Lab'); ?>
+	<?php include 'header.php'; print_header('Dataset - BigVid Lab'); ?>
 </head>
 <body>
-    <?php include 'navbar.php'; print_navbar(4); ?>
-    <div class="container">
-    </div>
-    <?php include 'footer.php' ?>
+	<?php include 'navbar.php'; print_navbar(4); ?>
+	<div class="container">
+		<div class="well">
+			<div class="list-group">
+				<div class="list-group-item">
+					<div class="row-picture">
+						<img class="bordered" src="img/VCDB.jpg">
+					</div>
+					<div class="row-content">
+						<p class="list-group-item-text"><a href="http://yugangjiang.info/research/VCDB/index.html">
+							VCDB: a large-scale database for partial copy detection in videos</a></p>
+						<p class="list-group-item-text"><a href="http://yugangjiang.info/publication/eccv14-VCDB.pdf">
+							ECCV 2014 paper</a></p>
+					</div>
+				</div>
+				<div class="list-group-item">
+					<div class="row-picture">
+						<img class="bordered" src="img/VideoEmotions.jpg">
+					</div>
+					<div class="row-content">
+						<p class="list-group-item-text"><a href="http://yugangjiang.info/research/VideoEmotions/index.html">
+							Dataset for predicting video emotions</a></p>
+						<p class="list-group-item-text"><a href="http://yugangjiang.info/publication/aaai14-emotions.pdf">
+							AAAI 2014 paper</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include 'footer.php' ?>
 </body>
 </html>
 
