b0698d5a8c3e8f1fba78c7e6d7c62cda55e9134e
Xi Wang <wandsea@gmail.com>
2014-12-26 15:56:42 +0800

    ....

--- demo.php
+++ demo.php
@@ -10,10 +10,10 @@
 	<div class="container">
 		<h4>Organizing Video Search Results to Adapted Semantic Hierarchies [related paper]</h4>
 		<video id="video1" class="video-js vjs-default-skin" controls
-		preload="auto" width="640" height="264" poster="MY_VIDEO_POSTER.jpg"
+		preload="auto" width="640" height="264" poster="poster.jpg"
 		data-setup="{}">
-		<source src="MY_VIDEO.mp4" type='video/mp4'>
-		<source src="MY_VIDEO.webm" type='video/webm'>
+		<source src="ocean.mp4" type='video/mp4'>
+		<source src="ocean.webm" type='video/webm'>
 		<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
 		</video>
 		<hr>
