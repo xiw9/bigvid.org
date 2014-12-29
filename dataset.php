397d8de651495efef00abf87c95b8b7c8b9da4ea
Xi Wang <wandsea@gmail.com>
2014-12-29 10:50:20 +0800

    dataset

--- dataset.php
+++ dataset.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<?php include 'header.php'; print_header('Dataset - BigVid Lab'); ?>
+	<?php include 'header.php'; print_header('Code & Dataset - BigVid Lab'); ?>
 </head>
 <body>
 	<?php include 'navbar.php'; print_navbar(4); ?>
@@ -10,33 +10,101 @@
   <TBODY>
     	<TR>
 	  <TD width=100 >
-	  <IMG width=88 src="img/VCDB.jpg">
+	  <IMG width=88 src="index.files/VCDB.jpg">
 	  </TD>
 	  <TD valign=center>
-			<a href="http://yugangjiang.info/research/VCDB/index.html" target=_blank>VCDB: a large-scale database for partial copy detection in videos</a><br>
-			<A href="http://yugangjiang.info/publication/eccv14-VCDB.pdf" target=_blank>ECCV 2014 paper</A> 			
+			<a href="research/VCDB/index.html" target=_blank>VCDB: a large-scale database for partial copy detection in videos</a><br>
+			<A href="publication/eccv14-VCDB.pdf" target=_blank>ECCV 2014 paper</A> 			
 		</TD>
 	</TR>
+	<hr>
     	<TR>
 	  <TD width=100 >
-	  <IMG width=88 src="img/VideoEmotions.jpg">
+	  <IMG width=88 src="index.files/VideoEmotions.jpg">
 	  </TD>
 	  <TD valign=center>
-			<a href="http://yugangjiang.info/research/VideoEmotions/index.html" target=_blank>Dataset for predicting video emotions</a><br>
-			<A href="http://yugangjiang.info/publication/aaai14-emotions.pdf" target=_blank>AAAI 2014 paper</A> 			
+			<a href="research/VideoEmotions/index.html" target=_blank>Dataset for predicting video emotions</a><br>
+			<A href="publication/aaai14-emotions.pdf" target=_blank>AAAI 2014 paper</A> 			
 		</TD>
 	</TR>
+	<hr>
 	<TR>
 	  <TD width=100 >
-	  <IMG width=88 src="img/interestingness.jpg">
+	  <IMG width=88 src="index.files/interestingness.jpg">
 	  </TD>
 	  <TD valign=center>
-			<a href="http://yugangjiang.info/research/interestingness/index.html" target=_blank>Dataset for predicting video interestingness</a><br>
-			<A href="http://yugangjiang.info/publication/aaai13-interestingness.pdf" target=_blank>AAAI 2013 paper</A> 			
+			<a href="research/interestingness/index.html" target=_blank>Dataset for predicting video interestingness</a><br>
+			<A href="publication/aaai13-interestingness.pdf" target=_blank>AAAI 2013 paper</A> 			
 		</TD>
 	</TR>
-	</TBODY>
+	<hr>
+	<TR>
+	  <TD width=100 >
+	  <IMG width=88 src="index.files/partattributes.png">
+	  </TD>
+	  <TD valign=center>
+			<a href="research/2012PartAttribute/index.html" target=_blank>Part-level attributes for visual recognition (source code)</a><br>
+			<A href="publication/eccv12-hybridparts.pdf" target=_blank>ECCV 2012 paper</A> 			
+		</TD>
+	</TR>
+	<hr>
+	<TR>
+	  <TD width=100 >
+	  <IMG width=88 src="index.files/consumerVid.jpg">
+	  </TD>
+	  <TD valign=center>
+			<a href="http://www.ee.columbia.edu/dvmm/CCV/" target=_blank>CCV: a benchmark dataset for consumer video analysis</a><br>			
+		</TD>
+	</TR>
+	<hr>
+	<TR>
+	  <TD width=100 >
+	  <img width=88 src="index.files/dasd_graph.jpg">
+	  </TD>
+	  <TD valign=center >
+	  	<A href="http://www.ee.columbia.edu/ln/dvmm/researchProjects/MultimediaIndexing/DASD/dasd.htm" target=_blank>
+	  		Domain adaptive semantic diffusion (DASD) for context-based visual annotation refinement</A><BR>
+	    <A href="publication/iccv09_yjiang.pdf" target=_blank>ICCV 2009 paper</A> | 
+	  <A href="http://www.ee.columbia.edu/ln/dvmm/researchProjects/MultimediaIndexing/DASD/DownloadRequest.php?set=code" target=_blank> source code</A>
+		</TD>
+  </TR>
+  <hr>
+  <TR>
+	  <TD width=100 >
+	  <IMG src="index.files/vireo374.gif">
+	  </TD>
+	  <TD valign=center>
+			<a href="http://vireo.cs.cityu.edu.hk/research/vireo374/" target=_blank>VIREO-374</a>: keypoint-based LSCOM semantic concept detectors
+			  <a href="index.files/VIREO-374_users.txt" target=_blank><font style="font-size: 8pt; color: #ff0000">see who has used it</font></a><BR>
+			  <a href="http://www.ee.columbia.edu/dvmm/CU-VIREO374/" target=_blank>CU-VIREO374</a>: fusing Columbia374 and VIREO374 for large scale semantic concept detection 
+	    	<a href="index.files/CU-VIREO374_users.txt" target=_blank><font style="font-size: 8pt; color: #ff0000">see who has used it</font></a>
+		</TD>
+	</TR>
+</TBODY>
 </TABLE>
+<h3>Selected other recent projects<h3>
+<TABLE cellSpacing=8 border=0 >
+  <TBODY>
+  <TR>
+	  <TD width=100 >
+	  <IMG width=88 height=75 src="index.files/QAIS-idea.jpg">
+	  </TD>
+	  <TD valign=center>
+			<a target=_blank href="research/2013QueryAdaptiveImageSearch/index.html">Query-adaptive image search with hash codes</a>
+			<br><a href="publication/TMM_queryadaptivesearch.pdf">IEEE TMM 2013 paper</a>			
+		</TD>
+	</TR>	 
+	 <hr> 
+  <TR>
+	  <TD width=100 >
+	  <IMG width=88 src="index.files/ViolentSceneDetection.jpg">
+	  </TD>
+	  <TD valign=center>
+			<a target=_blank href="research/2012MediaEval/index.html">Violent scene detection in movies</a>			
+		</TD>
+	</TR>	
+</TBODY>
+
 	</div>
 	<?php include 'footer.php' ?>
 </body>
--- demo.php
+++ demo.php
@@ -11,39 +11,43 @@
 <body>
 	<?php include 'navbar.php'; print_navbar(5); ?>
 	<div class="container">
-		<h4>Organizing Video Search Results to Adapted Semantic Hierarchies [related paper]</h4>
+		<h4>Organizing Video Search Results to Adapted Semantic Hierarchies</h4>
+		<p>[related paper]</p>
 		<video id="video1" class="video-js vjs-default-skin" controls
-		preload="auto" width="640" height="264" poster="poster.jpg"
+		preload="auto" width="640" height="360" poster="poster.jpg"
 		data-setup="{}">
 		<source src="ocean.mp4" type='video/mp4'>
 		<source src="ocean.webm" type='video/webm'>
 		<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
 		</video>
 		<hr>
-		<h4>Real-time Summarization of User-Generated Videos [related paper]</h4>
+		<h4>Real-time Summarization of User-Generated Videos</h4>
+		<p>[related paper]</p>
 		<video id="video2" class="video-js vjs-default-skin" controls
-		preload="auto" width="640" height="264" poster="MY_VIDEO_POSTER.jpg"
+		preload="auto" width="640" height="360" poster="poster.jpg"
 		data-setup="{}">
-		<source src="MY_VIDEO.mp4" type='video/mp4'>
-		<source src="MY_VIDEO.webm" type='video/webm'>
+		<source src="ocean.mp4" type='video/mp4'>
+		<source src="ocean.webm" type='video/webm'>
 		<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
 		</video>
 		<hr>
-		<h4>Fast Video Event Recognition and Its Application to Video Search [related paper]</h4>
+		<h4>Fast Video Event Recognition and Its Application to Video Search</h4>
+		<p>[related paper]</p>
 		<video id="video3" class="video-js vjs-default-skin" controls
-		preload="auto" width="640" height="264" poster="MY_VIDEO_POSTER.jpg"
+		preload="auto" width="640" height="360" poster="poster.jpg"
 		data-setup="{}">
-		<source src="MY_VIDEO.mp4" type='video/mp4'>
-		<source src="MY_VIDEO.webm" type='video/webm'>
+		<source src="ocean.mp4" type='video/mp4'>
+		<source src="ocean.webm" type='video/webm'>
 		<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
 		</video>
 		<hr>
-		<h4>Textually Describing Complex Video Contents [related paper]</h4>
+		<h4>Textually Describing Complex Video Contents</h4>
+		<p>[related paper]</p>
 		<video id="video4" class="video-js vjs-default-skin" controls
-		preload="auto" width="640" height="264" poster="MY_VIDEO_POSTER.jpg"
+		preload="auto" width="640" height="360" poster="poster.jpg"
 		data-setup="{}">
-		<source src="MY_VIDEO.mp4" type='video/mp4'>
-		<source src="MY_VIDEO.webm" type='video/webm'>
+		<source src="ocean.mp4" type='video/mp4'>
+		<source src="ocean.webm" type='video/webm'>
 		<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
 		</video>
 		<hr>
