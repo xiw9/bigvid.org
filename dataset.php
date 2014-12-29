bdc09439ce08c03295b03ad1c6a8c253a3d56ddb
Xi Wang <wandsea@gmail.com>
2014-12-29 14:59:21 +0800

    ...

--- dataset.php
+++ dataset.php
@@ -9,122 +9,113 @@
 <div class="row">
     <div class="col-xs-6">
         <h4>Datasets</h4>
-<div class="list-group list-dataset">
-    <div class="list-group-item">
-        <div class="row-picture">
-            <IMG src="index.files/VCDB.jpg">
-        </div>
-        <div class="row-content">
-			<p><a href="research/VCDB/index.html" >VCDB: a large-scale database for partial copy detection in videos</a></p>
-			<p><A href="publication/eccv14-VCDB.pdf" >ECCV 2014 paper</A> </p>		
-        </div>
-    </div>
-    <div class="list-group-separator"></div>
-    <div class="list-group-item">
-        <div class="row-picture">
-            <IMG src="index.files/VideoEmotions.jpg">
-        </div>
-        <div class="row-content">
-			<p><a href="research/VideoEmotions/index.html" >Dataset for predicting video emotions</a></p>
-			<p><A href="publication/aaai14-emotions.pdf" >AAAI 2014 paper</A> </p>
-        </div>
-    </div>
-    <div class="list-group-separator"></div>
-    <div class="list-group-item">
-        <div class="row-picture">
-            <IMG  src="index.files/nameface.png">
-        </div>
-        <div class="row-content">
-			<p><a href="http://vireo.cs.cityu.edu.hk/WebV-Cele/" >WebV-Cele: A Large-Scale Web Video Celebrities Dataset for Name-Face Association</a></p>
-			<p><A href="publication/JCST-nameface.pdf" >JCST 2014 paper</A> </p>
-        </div>
-    </div>
-    <div class="list-group-separator"></div>
-    <div class="list-group-item">
-        <div class="row-picture">
-            <IMG  src="index.files/interestingness.jpg">
-        </div>
-        <div class="row-content">
-			<p><a href="research/interestingness/index.html" >Dataset for predicting video interestingness</a></p>
-			<p><A href="publication/aaai13-interestingness.pdf" >AAAI 2013 paper</A> </p>
-        </div>
-    </div>
-    <div class="list-group-separator"></div>
-    <div class="list-group-item">
-        <div class="row-picture">
-            <IMG  src="index.files/partattributes.png">
-        </div>
-        <div class="row-content">
-			<p><a href="research/2012PartAttribute/index.html" >Part-level attributes for visual recognition (source code)</a></p>
-			<p><A href="publication/eccv12-hybridparts.pdf" >ECCV 2012 paper</A> </p>
-        </div>
-    </div>
-    <div class="list-group-separator"></div>
-    <div class="list-group-item">
-        <div class="row-picture">
-            <IMG  src="index.files/KSH.png">
-        </div>
-        <div class="row-content">
-			<p><a href="http://www.ee.columbia.edu/ln/dvmm/downloads/WeiKSHCode/dlform.htm" >Supervised Hashing with Kernels</a></p>
-			<p><A href="publication/CVPR12-KSH.pdf" >CVPR 2012 paper</A> </p>
-        </div>
-    </div>
-    <div class="list-group-separator"></div>
-    <div class="list-group-item">
-        <div class="row-picture">
-            <IMG  src="index.files/consumerVid.jpg">
-        </div>
-        <div class="row-content">
-			<p><a href="http://www.ee.columbia.edu/dvmm/CCV/" >CCV: a benchmark dataset for consumer video analysis</a></p>
-            <p><A href="publication/icmr11-consumervideo.pdf" >ICMR 2011 paper</A> </p>
+        <div class="list-group list-dataset">
+            <div class="list-group-item">
+                <div class="row-picture">
+                    <IMG src="index.files/VCDB.jpg">
+                </div>
+                <div class="row-content">
+        			<p><a href="research/VCDB/index.html" >VCDB: a large-scale database for partial copy detection in videos</a></p>
+        			<p><A href="publication/eccv14-VCDB.pdf" >ECCV 2014 paper</A> </p>		
+                </div>
+            </div>
+            <div class="list-group-separator"></div>
+            <div class="list-group-item">
+                <div class="row-picture">
+                    <IMG src="index.files/VideoEmotions.jpg">
+                </div>
+                <div class="row-content">
+        			<p><a href="research/VideoEmotions/index.html" >Dataset for predicting video emotions</a></p>
+        			<p><A href="publication/aaai14-emotions.pdf" >AAAI 2014 paper</A> </p>
+                </div>
+            </div>
+            <div class="list-group-separator"></div>
+            <div class="list-group-item">
+                <div class="row-picture">
+                    <IMG  src="index.files/nameface.png">
+                </div>
+                <div class="row-content">
+        			<p><a href="http://vireo.cs.cityu.edu.hk/WebV-Cele/" >WebV-Cele: A Large-Scale Web Video Celebrities Dataset for Name-Face Association</a></p>
+        			<p><A href="publication/JCST-nameface.pdf" >JCST 2014 paper</A> </p>
+                </div>
+            </div>
+            <div class="list-group-separator"></div>
+            <div class="list-group-item">
+                <div class="row-picture">
+                    <IMG  src="index.files/interestingness.jpg">
+                </div>
+                <div class="row-content">
+        			<p><a href="research/interestingness/index.html" >Dataset for predicting video interestingness</a></p>
+        			<p><A href="publication/aaai13-interestingness.pdf" >AAAI 2013 paper</A> </p>
+                </div>
+            </div>
+
+            <div class="list-group-separator"></div>
+            <div class="list-group-item">
+                <div class="row-picture">
+                    <IMG  src="index.files/consumerVid.jpg">
+                </div>
+                <div class="row-content">
+        			<p><a href="http://www.ee.columbia.edu/dvmm/CCV/" >CCV: a benchmark dataset for consumer video analysis</a></p>
+                    <p><A href="publication/icmr11-consumervideo.pdf" >ICMR 2011 paper</A> </p>
+                </div>
+            </div>
+            <div class="list-group-separator"></div>
+
+            <div class="list-group-item">
+                <div class="row-picture">
+                    <IMG src="index.files/vireo374.gif">
+                </div>
+                <div class="row-content">
+        			<p><a href="http://vireo.cs.cityu.edu.hk/research/vireo374/" >VIREO-374</a>: keypoint-based LSCOM semantic concept detectors
+        			  <a href="index.files/VIREO-374_users.txt" ><font style="font-size: 8pt; color: #ff0000">see who has used it</font></a></p>
+        			 <p> <a href="http://www.ee.columbia.edu/dvmm/CU-VIREO374/" >CU-VIREO374</a>: fusing Columbia374 and VIREO374 for large scale semantic concept detection 
+        	    	<a href="index.files/CU-VIREO374_users.txt" ><font style="font-size: 8pt; color: #ff0000">see who has used it</font></a></p>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="list-group-separator"></div>
-    <div class="list-group-item">
-        <div class="row-picture">
-            <img  src="index.files/dasd_graph.jpg">
+    <div class="col-xs-6">
+        <h4>Codes</h4>
+        <div class="list-group list-dataset">
+            <div class="list-group-separator"></div>
+            <div class="list-group-item">
+                <div class="row-picture">
+                    <IMG  src="index.files/partattributes.png">
+                </div>
+                <div class="row-content">
+                    <p><a href="research/2012PartAttribute/index.html" >Part-level attributes for visual recognition (source code)</a></p>
+                    <p><A href="publication/eccv12-hybridparts.pdf" >ECCV 2012 paper</A> </p>
+                </div>
+            </div>
+            <div class="list-group-separator"></div>
+            <div class="list-group-item">
+                <div class="row-picture">
+                    <IMG  src="index.files/KSH.png">
+                </div>
+                <div class="row-content">
+                    <p><a href="http://www.ee.columbia.edu/ln/dvmm/downloads/WeiKSHCode/dlform.htm" >Supervised Hashing with Kernels</a></p>
+                    <p><A href="publication/CVPR12-KSH.pdf" >CVPR 2012 paper</A> </p>
+                </div>
+            </div>
+            <div class="list-group-separator"></div>
+            <div class="list-group-item">
+                <div class="row-picture">
+                    <img  src="index.files/dasd_graph.jpg">
+                </div>
+                <div class="row-content">
+                    <p> <A href="http://www.ee.columbia.edu/ln/dvmm/researchProjects/MultimediaIndexing/DASD/dasd.htm" >
+                    Domain adaptive semantic diffusion (DASD) for context-based visual annotation refinement</A></p>
+                    <p><A href="publication/iccv09_yjiang.pdf" >ICCV 2009 paper</A> | 
+                    <A href="http://www.ee.columbia.edu/ln/dvmm/researchProjects/MultimediaIndexing/DASD/DownloadRequest.php?set=code" > source code</A></p>
+                </div>
+            </div>
         </div>
-        <div class="row-content">
-			<p>	<A href="http://www.ee.columbia.edu/ln/dvmm/researchProjects/MultimediaIndexing/DASD/dasd.htm" >
-	  		Domain adaptive semantic diffusion (DASD) for context-based visual annotation refinement</A></p>
-	    	<p><A href="publication/iccv09_yjiang.pdf" >ICCV 2009 paper</A> | 
-	  		<A href="http://www.ee.columbia.edu/ln/dvmm/researchProjects/MultimediaIndexing/DASD/DownloadRequest.php?set=code" > source code</A></p>
-        </div>
+
     </div>
-    <div class="list-group-separator"></div>
-    <div class="list-group-item">
-        <div class="row-picture">
-            <IMG src="index.files/vireo374.gif">
-        </div>
-        <div class="row-content">
-			<p><a href="http://vireo.cs.cityu.edu.hk/research/vireo374/" >VIREO-374</a>: keypoint-based LSCOM semantic concept detectors
-			  <a href="index.files/VIREO-374_users.txt" ><font style="font-size: 8pt; color: #ff0000">see who has used it</font></a></p>
-			 <p> <a href="http://www.ee.columbia.edu/dvmm/CU-VIREO374/" >CU-VIREO374</a>: fusing Columbia374 and VIREO374 for large scale semantic concept detection 
-	    	<a href="index.files/CU-VIREO374_users.txt" ><font style="font-size: 8pt; color: #ff0000">see who has used it</font></a></p>
-        </div>
-    </div>
-    <div class="list-group-separator"></div>
-    <div class="list-group-item">
-        <div class="row-picture">
-            <IMG  height=75 src="index.files/QAIS-idea.jpg">
-        </div>
-        <div class="row-content">
-			<p><a  href="research/2013QueryAdaptiveImageSearch/index.html">Query-adaptive image search with hash codes</a></p>
-			<p><a href="publication/TMM_queryadaptivesearch.pdf">IEEE TMM 2013 paper</a></p>
-        </div>
-    </div>
-    <div class="list-group-separator"></div>
-    <div class="list-group-item">
-        <div class="row-picture">
-            <IMG  src="index.files/ViolentSceneDetection.jpg">
-        </div>
-        <div class="row-content">
-			<p><a  href="research/2012MediaEval/index.html">Violent scene detection in movies</a><p>
-        </div>
-    </div>
+
 </div>
-</div>
-</div>
+
 
 	</div>
 	<?php include 'footer.php' ?>
