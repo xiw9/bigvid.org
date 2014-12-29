ab29e2cda4c8f6256f6835ba687846c45e497559
Xi Wang <wandsea@gmail.com>
2014-12-29 11:42:08 +0800

    add dataset

--- dataset.php
+++ dataset.php
@@ -7,6 +7,8 @@
 	<?php include 'navbar.php'; print_navbar(4); ?>
 	<div class="container">
 
+
+
 <TABLE cellSpacing=8 border=0 >
   <TBODY>
     	<TR>
@@ -14,8 +16,8 @@
 	  <IMG width=88 src="index.files/VCDB.jpg">
 	  </TD>
 	  <TD valign=center>
-			<a href="research/VCDB/index.html" target=_blank>VCDB: a large-scale database for partial copy detection in videos</a><br>
-			<A href="publication/eccv14-VCDB.pdf" target=_blank>ECCV 2014 paper</A> 			
+			<a href="research/VCDB/index.html" >VCDB: a large-scale database for partial copy detection in videos</a><br>
+			<A href="publication/eccv14-VCDB.pdf" >ECCV 2014 paper</A> 			
 		</TD>
 	</TR>
 	
@@ -24,18 +26,26 @@
 	  <IMG width=88 src="index.files/VideoEmotions.jpg">
 	  </TD>
 	  <TD valign=center>
-			<a href="research/VideoEmotions/index.html" target=_blank>Dataset for predicting video emotions</a><br>
-			<A href="publication/aaai14-emotions.pdf" target=_blank>AAAI 2014 paper</A> 			
+			<a href="research/VideoEmotions/index.html" >Dataset for predicting video emotions</a><br>
+			<A href="publication/aaai14-emotions.pdf" >AAAI 2014 paper</A> 			
 		</TD>
 	</TR>
-	
+    	<TR>
+	  <TD width=100 >
+	  <IMG width=88 src="index.files/VideoEmotions.jpg">
+	  </TD>
+	  <TD valign=center>
+			<a href="http://vireo.cs.cityu.edu.hk/WebV-Cele/" >WebV-Cele: A Large-Scale Web Video Celebrities Dataset for Name-Face Association</a><br>
+			<A href="publication/JCST-nameface.pdf" >Journal of Computer Science and Technology</A> 			
+		</TD>
+	</TR>	
 	<TR>
 	  <TD width=100 >
 	  <IMG width=88 src="index.files/interestingness.jpg">
 	  </TD>
 	  <TD valign=center>
-			<a href="research/interestingness/index.html" target=_blank>Dataset for predicting video interestingness</a><br>
-			<A href="publication/aaai13-interestingness.pdf" target=_blank>AAAI 2013 paper</A> 			
+			<a href="research/interestingness/index.html" >Dataset for predicting video interestingness</a><br>
+			<A href="publication/aaai13-interestingness.pdf" >AAAI 2013 paper</A> 			
 		</TD>
 	</TR>
 	
@@ -44,17 +54,25 @@
 	  <IMG width=88 src="index.files/partattributes.png">
 	  </TD>
 	  <TD valign=center>
-			<a href="research/2012PartAttribute/index.html" target=_blank>Part-level attributes for visual recognition (source code)</a><br>
-			<A href="publication/eccv12-hybridparts.pdf" target=_blank>ECCV 2012 paper</A> 			
+			<a href="research/2012PartAttribute/index.html" >Part-level attributes for visual recognition (source code)</a><br>
+			<A href="publication/eccv12-hybridparts.pdf" >ECCV 2012 paper</A> 			
 		</TD>
 	</TR>
-	
+	<TR>
+	  <TD width=100 >
+	  <IMG width=88 src="index.files/partattributes.png">
+	  </TD>
+	  <TD valign=center>
+			<a href="http://www.ee.columbia.edu/ln/dvmm/downloads/WeiKSHCode/dlform.htm" >Supervised Hashing with Kernels</a><br>
+			<A href="publication/CVPR12-KSH.pdf" >CVPR 2012 paper</A> 			
+		</TD>
+	</TR>	
 	<TR>
 	  <TD width=100 >
 	  <IMG width=88 src="index.files/consumerVid.jpg">
 	  </TD>
 	  <TD valign=center>
-			<a href="http://www.ee.columbia.edu/dvmm/CCV/" target=_blank>CCV: a benchmark dataset for consumer video analysis</a><br>			
+			<a href="http://www.ee.columbia.edu/dvmm/CCV/" >CCV: a benchmark dataset for consumer video analysis</a><br>			
 		</TD>
 	</TR>
 	
@@ -63,10 +81,10 @@
 	  <img width=88 src="index.files/dasd_graph.jpg">
 	  </TD>
 	  <TD valign=center >
-	  	<A href="http://www.ee.columbia.edu/ln/dvmm/researchProjects/MultimediaIndexing/DASD/dasd.htm" target=_blank>
+	  	<A href="http://www.ee.columbia.edu/ln/dvmm/researchProjects/MultimediaIndexing/DASD/dasd.htm" >
 	  		Domain adaptive semantic diffusion (DASD) for context-based visual annotation refinement</A><BR>
-	    <A href="publication/iccv09_yjiang.pdf" target=_blank>ICCV 2009 paper</A> | 
-	  <A href="http://www.ee.columbia.edu/ln/dvmm/researchProjects/MultimediaIndexing/DASD/DownloadRequest.php?set=code" target=_blank> source code</A>
+	    <A href="publication/iccv09_yjiang.pdf" >ICCV 2009 paper</A> | 
+	  <A href="http://www.ee.columbia.edu/ln/dvmm/researchProjects/MultimediaIndexing/DASD/DownloadRequest.php?set=code" > source code</A>
 		</TD>
   </TR>
   
@@ -75,10 +93,10 @@
 	  <IMG src="index.files/vireo374.gif">
 	  </TD>
 	  <TD valign=center>
-			<a href="http://vireo.cs.cityu.edu.hk/research/vireo374/" target=_blank>VIREO-374</a>: keypoint-based LSCOM semantic concept detectors
-			  <a href="index.files/VIREO-374_users.txt" target=_blank><font style="font-size: 8pt; color: #ff0000">see who has used it</font></a><BR>
-			  <a href="http://www.ee.columbia.edu/dvmm/CU-VIREO374/" target=_blank>CU-VIREO374</a>: fusing Columbia374 and VIREO374 for large scale semantic concept detection 
-	    	<a href="index.files/CU-VIREO374_users.txt" target=_blank><font style="font-size: 8pt; color: #ff0000">see who has used it</font></a>
+			<a href="http://vireo.cs.cityu.edu.hk/research/vireo374/" >VIREO-374</a>: keypoint-based LSCOM semantic concept detectors
+			  <a href="index.files/VIREO-374_users.txt" ><font style="font-size: 8pt; color: #ff0000">see who has used it</font></a><BR>
+			  <a href="http://www.ee.columbia.edu/dvmm/CU-VIREO374/" >CU-VIREO374</a>: fusing Columbia374 and VIREO374 for large scale semantic concept detection 
+	    	<a href="index.files/CU-VIREO374_users.txt" ><font style="font-size: 8pt; color: #ff0000">see who has used it</font></a>
 		</TD>
 	</TR>
 </TBODY>
@@ -91,7 +109,7 @@
 	  <IMG width=88 height=75 src="index.files/QAIS-idea.jpg">
 	  </TD>
 	  <TD valign=center>
-			<a target=_blank href="research/2013QueryAdaptiveImageSearch/index.html">Query-adaptive image search with hash codes</a>
+			<a  href="research/2013QueryAdaptiveImageSearch/index.html">Query-adaptive image search with hash codes</a>
 			<br><a href="publication/TMM_queryadaptivesearch.pdf">IEEE TMM 2013 paper</a>			
 		</TD>
 	</TR>	 
@@ -101,7 +119,7 @@
 	  <IMG width=88 src="index.files/ViolentSceneDetection.jpg">
 	  </TD>
 	  <TD valign=center>
-			<a target=_blank href="research/2012MediaEval/index.html">Violent scene detection in movies</a>			
+			<a  href="research/2012MediaEval/index.html">Violent scene detection in movies</a>			
 		</TD>
 	</TR>	
 </TBODY>
